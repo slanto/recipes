d87c72256b41482edfa6f44b551a6c75827bd6da
Slawomir Antos <santos@cybervadis.com>
2017-11-30 13:01:45 +0100

    Displaying recipe details

--- src/app/recipies/recipie-list/recipie-list.component.ts
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipie-list',
   templateUrl: './recipie-list.component.html',
   styleUrls: ['./recipie-list.component.css']
 })
 export class RecipieListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simple test',
+    'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
+    new Recipe('A test recipe', 'This is simple test',
+    'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg')
+  ];
 
   constructor() { }
 
