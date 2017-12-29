79035fe148ee1c73bdfbc1c6edb65d74ebde29b2
slanto <slawek.antos@gmail.com>
2017-12-29 16:21:35 +0100

    Setting up routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {RecipiesComponent} from './recipies/recipies.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes', component: RecipiesComponent },
+    { path: 'shopping-list', component: ShoppingListComponent  },
+    
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
